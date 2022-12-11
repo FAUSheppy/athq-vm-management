5f7ce0e31499d5cbeacef99a4e8bee055a86dd2d
Yannik Schmidt <sheppy@atlantishq.de>
2022-12-11 02:57:33 +0100

    feat: print static acl

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import vm
 if __name__ == "__main__":
 
     FILE = "vms.json"
+    print(vm.HA_PROXY_STATIC_ACLS)
     with open(FILE) as f:
         jsonList = json.load(f)
         vmList = [ vm.VM(obj) for obj in jsonList ]
         
         for vmo in vmList:
             [ print(c) for c in vmo.dumpHAProxyComponents()]
+
