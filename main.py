feb1d8c42d4bb09caf1a38319deeb1fb70c52f3f
Sheppy <sheppy@atlantishq.de>
2022-12-21 14:33:00 +0100

    feat: nginx main conf

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 import vm
+import sys
+import jinja2
 
 ACME_CONTENT = '''
 location /.well-known/acme-challenge/ {
@@ -10,7 +12,7 @@ location /.well-known/acme-challenge/ {
 
 if __name__ == "__main__":
 
-    FILE = "vms.json"
+    FILE = "./config/vms.json"
     with open(FILE) as f:
         jsonList = json.load(f)
         vmList = []
@@ -20,19 +22,19 @@ if __name__ == "__main__":
                 vmList.append(vmo)
             except ValueError as e:
                 print(e, file=sys.stderr)
-       
-        with open("./build/stream_include.conf", "w") as f:
+
+        with open("/etc/nginx/stream_include.conf", "w") as f:
             for vmo in vmList:
                 [ f.write(c) for c in vmo.dumpStreamComponents()]
 
-        with open("./build/http_include.conf", "w") as f:
+        with open("/etc/nginx/http_include.conf", "w") as f:
             for vmo in vmList:
                 [ f.write(c) for c in vmo.dumpServerComponents()]
 
-        with open("./build/acme-challenge.conf", "w") as f:
+        with open("/etc/nginx/acme-challenge.conf", "w") as f:
             f.write(ACME_CONTENT)
 
-        with open("./build/cert.sh", "w") as f:
+        with open("/etc/nginx/cert.sh", "w") as f:
 
             f.write("certbot certonly --webroot -w /var/www \\")
             domains = []
@@ -47,3 +49,13 @@ if __name__ == "__main__":
                 f.write("    -d {} \\".format(d))
 
             f.write("--rsa-key-size 2048 --expand")
+
+        with open("/etc/nginx/nginx.conf", "w") as f:
+
+            with open("./config/nginx.json") as j:
+                nginxJson = json.load(j)
+                env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath="./templates"))
+                template = env.get_template("nginx.conf.j2")
+                content = template.render(nginxJson)
+
+                f.write(content)
